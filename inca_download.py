e0a0b4df8d93132e244bcd817d958301af5eb7cb
oscipal <otto.scipal@gmail.com>
2025-07-14 16:52:45 +0200

    INCA

--- inca_download.py
+++ inca_download.py
@@ -3,11 +3,12 @@ import os
 import sys
 
 def download_data(var):
+    print(var)
     url = f"https://public.hub.geosphere.at/datahub/resources/inca-v1-1h-1km/filelisting/{var}/INCAL_HOURLY_{var}_202506.nc"
     filename = os.path.basename(url)
     base = "/tmp"
     urlretrieve(url, os.path.join(base,filename))
-    print(var)
+    
                               
 if __name__ == "__main__":
     var = sys.argv[1]
